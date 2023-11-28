a36b1e23e3d9140332d70424ffe1f3057b5737c3
Bishal487 <bishal.kunwar487@gmail.com>
2023-11-28 10:42:04 -0500

    backend init for shareme social media app

---
+++ schemas/index.js
@@ -0,0 +1,8 @@
+import user from "./user";
+import comments from "./comments";
+import save from "./save";
+import postedBy from "./postedBy";
+import pin from "./pin";
+
+
+export const schemaTypes = [user, pin, comments, save,postedBy]
---
+++ schemas/user.js
@@ -0,0 +1,13 @@
+export default {
+    name: 'user',
+    type: 'document',
+    title: 'user',
+    fields: [
+        {
+            name: 'userName',
+            title: 'userName',
+            type: 'string'
+        },
+    ]
+
+}
